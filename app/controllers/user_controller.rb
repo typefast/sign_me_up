242fa1798d5f6ca4f896738593b97fceeb43051a
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-29 21:37:05 +0000

    Add method logic

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,21 @@
 class UserController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      flash[:success] = "Thank You, We will keep you updated!"
+      redirect_to '/new'
+    else
+      flash[:error] = "There was an error, make sure a name and valid email have been entered."
+      render :new
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
